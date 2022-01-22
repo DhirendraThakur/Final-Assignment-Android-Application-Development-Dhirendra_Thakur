427b387b646a0d9703cfe0df608ae2f89997cb7e
Dhirendra Thakur <drthunk@gmail.com>
2022-01-22 15:40:05 +0545

    registration page has been added

--- lib/response/response.dart
+++ lib/response/response.dart
@@ -2,13 +2,11 @@ class ResponseUser{
   bool? success;
   String? token;
 
-
 ResponseUser({this.success, this.token});
 factory ResponseUser.fromjson(Map<String, dynamic>json){
 return ResponseUser(
   success: json['Success'],
   token: json['token']
 );
-
 }
 }
