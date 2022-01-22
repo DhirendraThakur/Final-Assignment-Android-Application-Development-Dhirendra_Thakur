d7889cb9d1cb7cec80f8aefc14a982e0e9c75782
Dhirendra Thakur <drthunk@gmail.com>
2022-01-22 15:22:05 +0545

    http folder has been created

---
+++ lib/response/response.dart
@@ -0,0 +1,13 @@
+class ResponseUser{
+  bool? success;
+  String? token;
+
+
+ResponseUser({this.success, this.token});
+factory ResponseUser.fromjson(Map<String, dynamic>json){
+return ResponseUser(
+  success: json['Success'],
+  token: json['token']
+);
+}
+}
