a7294349e012ed98fe542032b16e6e08c1602f80
Dhirendra Thakur <drthunk@gmail.com>
2022-02-25 00:22:13 +0545

    set routes and response formating

---
+++ lib/model/user.dart
@@ -0,0 +1,7 @@
+class User{
+  final String? username;
+  final String? password;
+  final String? phone;
+  final String? address;
+  User ({this.username, this.password, this.address, this.phone});
+}
--- lib/response/response.dart
+++ lib/response/response.dart
@@ -1,12 +1,10 @@
-class ResponseUser{
+class ResponseUser {
   bool? success;
   String? token;
 
-ResponseUser({this.success, this.token});
-factory ResponseUser.fromjson(Map<String, dynamic>json){
-return ResponseUser(
-  success: json['Success'],
-  token: json['token']
-);
-}
+  ResponseUser({this.success, this.token});
+
+  factory ResponseUser.fromjson(Map<String, dynamic> json) {
+    return ResponseUser(success: json['Success'], token: json['token']);
+  }
 }
