08f1264a0c8fc5d2a184a55d9511d1a02dfbf14b
Dhirendra Thakur <drthunk@gmail.com>
2022-02-27 00:15:18 +0545

    add supplement screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:fitness/screen/dashboard.dart';
 import 'package:fitness/screen/displayproduct.dart';
+import 'package:fitness/screen/displaysupplement.dart';
 import 'package:fitness/screen/firstpage.dart';
 import 'package:fitness/screen/login.dart';
 import 'package:fitness/screen/product.dart';
 import 'package:fitness/screen/register.dart';
-import 'package:fitness/screen/updateproduct.dart';
+import 'package:fitness/screen/supplement_1.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,6 +26,8 @@ class MyApp extends StatelessWidget {
         '/register': (context) => RegisterScreen(),
         '/product/add': (context) => SupplementAdd(),
         "/product/show": (context) => DisplayProduct(),
+        "/supplement/add": (context) => SuppleAdd(),
+        "/supplement/show": (context) => DisplaySupplement(),
       },
       // home: RegisterScreen(),
       // home : Dashboard(),
--- lib/screen/supplement_1.dart
+++ lib/screen/supplement_1.dart
@@ -1,164 +1,187 @@
 import 'dart:io';
 
-import 'package:fitness/http/httpproduct.dart';
 import 'package:fitness/http/httpsupplement.dart';
 import 'package:fitness/model/supplement.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:fitness/model/product.dart';
 
 class SuppleAdd extends StatefulWidget {
-  const SuppleAdd({ Key? key }) : super(key: key);
+  const SuppleAdd({Key? key}) : super(key: key);
 
   @override
   _SuppleAddState createState() => _SuppleAddState();
 }
 
 class _SuppleAddState extends State<SuppleAdd> {
-final _formkey = GlobalKey<FormState>();
- File? _image;
- String supplementname="";
- String supplementtype="";
- String supplementrate="";
- //String productdesc="";
-@override
-void initState(){
-  super.initState();
-  _image=null;
-}
+  final _formkey = GlobalKey<FormState>();
+  File? _image;
+  String supplementname = "";
+  String supplementtype = "";
+  String supplementrate = "";
 
-//method to open image from gallery
-_imageFromGallery () async{
-  final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-  setState(() {
-    _image = File(image!.path);
-  });
-}
+  @override
+  void initState() {
+    super.initState();
+    _image = null;
+  }
 
-//method to open image from camera
-_imageFromCamera () async{
-  final image = await ImagePicker().pickImage(source: ImageSource.camera);
-  setState(() {
-    _image = File(image!.path);
-  });
-}
+  //method to open image from gallery
+  _imageFromGallery() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
 
-
-
+  //method to open image from camera
+  _imageFromCamera() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-       appBar : AppBar(title: Text('Add Supplement'),),
-      body:  SingleChildScrollView(
+      appBar: AppBar(
+        title: Text('Add Supplement'),
+      ),
+      body: SingleChildScrollView(
         padding: EdgeInsets.all(20),
-        child: Form(child: Column(children: [
-          Container(alignment: Alignment.center,
-          child: Text('Add Supplement Details', style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
-            letterSpacing:  5.0
-          ),),
-           
-          ),
-          Stack(children: [
-            CircleAvatar(radius: 50,
-            backgroundImage: _image == null 
-            ?const AssetImage('images/profile_image.png') as ImageProvider : FileImage(_image!),
-
-            child: InkWell(onTap: (){
-              showModalBottomSheet(context: context, builder: (builder)=> bottomSheet());
-            },
-            child: const Icon(Icons.upload,
-            color: Colors.red, size:  30,),
-            ),
-            
-            
-            )
-          ],),
-           TextFormField(
-onSaved: (value){
-  supplementname = value!; 
-},
-maxLength: 30,
-decoration:  InputDecoration(labelText: 'Supplement Name',
-hintText: 'Enter Supplement Name'),
-),
-
-TextFormField(
-onSaved: (value){
-  supplementrate = value!; 
-},
-maxLength: 30,
-decoration:  InputDecoration(labelText: 'Supplement Rate',
-hintText: 'Enter Supplement Rate'),
-),
-
-
-TextFormField(
-onSaved: (value){
-  supplementtype = value!; 
-},
-maxLength: 30,
-decoration:  InputDecoration(labelText: 'Supplement Type',
-hintText: 'Enter Supplement Type'),
-),
-
-  SizedBox(height: 30.0,),
-    ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        minimumSize: Size(double.infinity, 50)
+        child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Add Supplement Details',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                        letterSpacing: 5.0),
+                  ),
+                ),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: _image == null
+                          ? const AssetImage('images/profile_image.png')
+                              as ImageProvider
+                          : FileImage(_image!),
+                      child: InkWell(
+                        onTap: () {
+                          showModalBottomSheet(
+                              context: context,
+                              builder: (builder) => bottomSheet(context,
+                                  _imageFromGallery, _imageFromCamera));
+                        },
+                        child: const Icon(
+                          Icons.upload,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                TextFormField(
+                  onSaved: (value) {
+                    supplementname = value!;
+                  },
+                  maxLength: 30,
+                  decoration: InputDecoration(
+                    labelText: 'Supplement Name',
+                    hintText: 'Enter Supplement Name',
+                  ),
+                ),
+                TextFormField(
+                  onSaved: (value) {
+                    supplementrate = value!;
+                  },
+                  maxLength: 30,
+                  decoration: InputDecoration(
+                      labelText: 'Supplement Rate',
+                      hintText: 'Enter Supplement Rate'),
+                ),
+                TextFormField(
+                  onSaved: (value) {
+                    supplementtype = value!;
+                  },
+                  maxLength: 30,
+                  decoration: InputDecoration(
+                      labelText: 'Supplement Type',
+                      hintText: 'Enter Supplement Type'),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(double.infinity, 50),
+                    ),
+                    onPressed: () {
+                      _formkey.currentState!.save();
+                      Supplement supplement = Supplement(
+                        supplementname: supplementname,
+                        supplementtype: supplementtype,
+                        supplementrate: supplementrate,
+                      );
+                      print(supplement);
+                      httpSupplement().registerSupplement(supplement);
+                    },
+                    child: Text('Add')),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
+                    onPressed: () {},
+                    child: Text('View Supplement')),
+              ],
+            )),
       ),
-      onPressed: (){
-        _formkey.currentState!.save();
-        Supplement supplement = Supplement(
-          supplementname: supplementname,
-          supplementtype: supplementtype,
-          supplementrate: supplementrate,
-          //productdesc: productdesc
-          
-          );
-          httpSupplement().registerSupplement(supplement,_image);
-        
-
-      }, child: Text('Add')),
-      const SizedBox(height: 10,),
-      ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          minimumSize: const Size(double.infinity, 50),
-        ),
-        onPressed: (){
-          
-
-        }, child: Text('View Supplement')),
-
-        ],)),
-      ),
-
-
-
-
-
     );
   }
 }
 
- Widget bottomSheet(){
-    return Container(child: Column(children: [
-      Text('Choose picture',
-     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24), 
-     ),
-     const SizedBox( height: 20,), 
-     Row(
-       mainAxisAlignment: MainAxisAlignment.center,
-       children: [
-         ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.image), label: Text('Camera')),
-         ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.image), label: Text('Gallery'))
-       ],
-     )
-
-    ],),);
-  }
-  
+Widget bottomSheet(BuildContext context, Function() imageFromGallery,
+    Function() imageFromCamera) {
+  return Container(
+    child: Column(
+      children: [
+        Text(
+          'Choose picture',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {
+                  imageFromCamera();
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.image),
+                label: Text('Camera')),
+            ElevatedButton.icon(
+                onPressed: () {
+                  imageFromGallery();
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.image),
+                label: Text('Gallery'))
+          ],
+        )
+      ],
+    ),
+  );
+}
